d910c3072928f7956d7fc537634eb31007e50415
Jacques Lucke <mail@jlucke.com>
2020-11-07 14:46:04 +0100

    start using typescript

---
+++ web-frontend/src/index.ts
@@ -0,0 +1,2 @@
+const x: string = 'hello world 2';
+console.log(x);
--- web-frontend/webpack.config.js
+++ web-frontend/webpack.config.js
@@ -3,10 +3,22 @@ const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.ts',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
   },
   plugins: [
     new CleanWebpackPlugin(),
