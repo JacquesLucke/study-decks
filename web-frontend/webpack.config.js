ddfae9b5fd08be7f12e34ec638466cb1df57eb7f
Jacques Lucke <mail@jlucke.com>
2020-11-07 14:11:54 +0100

    enable html-webpack-plugin

--- web-frontend/webpack.config.js
+++ web-frontend/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -9,5 +10,6 @@ module.exports = {
     },
     plugins: [
         new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin(),
     ],
 };
