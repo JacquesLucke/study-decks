8fcec5a452630f0525d6f17053a262ebe1555d41
Jacques Lucke <mail@jlucke.com>
2020-11-07 15:04:38 +0100

    start using react

--- web-frontend/src/index.ts
+++ web-frontend/src/index.ts
@@ -1,2 +1,20 @@
-const x: string = 'hello world 2';
-console.log(x);
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+
+class MyButton extends React.Component<{}, {liked: boolean}> {
+  constructor(props) {
+    super(props);
+    this.state = {liked: false};
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return React.createElement('button', {onClick: () => this.setState({liked: true})}, 'Like');
+  }
+}
+
+const container = document.querySelector('#maindiv');
+ReactDom.render(React.createElement(MyButton), container);
