e44596f8b8d58281d382f0e0425c9280d5c4d7ad
Jacques Lucke <mail@jlucke.com>
2020-11-22 14:48:24 +0100

    initial basic website structure

--- web-frontend/src/index.tsx
+++ web-frontend/src/index.tsx
@@ -1,21 +1,68 @@
 import * as React from "react";
 import * as ReactDom from "react-dom";
-import "./mystyle.scss";
+import "./app-style.scss";
 
-class MyButton extends React.Component<{}, { liked: boolean }> {
-  constructor(props) {
-    super(props);
-    this.state = { liked: false };
-  }
-
+class AppHeader extends React.Component {
   render() {
-    if (this.state.liked) {
-      return "You liked this.";
-    }
-
-    return <button onClick={() => this.setState({ liked: true })}>Like</button>;
+    return (
+      <div className="app-header">
+        <div className="app-header-content">
+          <div className="app-header-name">Study Decks</div>
+          <div className="app-header-right">
+            <nav>
+              <ul>
+                <li>Log In</li>
+                <li>About</li>
+                <li>Home</li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
-const container = document.querySelector("#maindiv");
-ReactDom.render(<MyButton />, container);
+class AppBodyLeftSidebar extends React.Component {
+  render() {
+    return <div className="app-body-left-sidebar">Left Sidebar</div>;
+  }
+}
+
+class AppBodyCenter extends React.Component {
+  render() {
+    return <div className="app-body-center">Center</div>;
+  }
+}
+
+class AppBpdyRightSidebar extends React.Component {
+  render() {
+    return <div className="app-body-right-sidebar">Right Sidebar</div>;
+  }
+}
+
+class AppBody extends React.Component {
+  render() {
+    return (
+      <div className="app-body">
+        <AppBodyLeftSidebar />
+        <AppBodyCenter />
+        <AppBpdyRightSidebar />
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <AppHeader />
+        <AppBody />
+      </div>
+    );
+  }
+}
+
+const container = document.querySelector("#app-div");
+ReactDom.render(<App />, container);
