e0623338c942ac2dc3f3c866dba7e44fc43cede6
Jacques Lucke <mail@jlucke.com>
2020-11-22 15:05:30 +0100

    add some more mockups

--- web-frontend/src/index.tsx
+++ web-frontend/src/index.tsx
@@ -23,21 +23,61 @@ class AppHeader extends React.Component {
   }
 }
 
+class DeckInfo extends React.Component {
+  render() {
+    return (
+      <div className="deck-info">
+        <p>You are learning:</p>
+        <p>How to do X?</p>
+        <hr />
+        <p>Author: Some Name</p>
+        <p>Questions: XX</p>
+        <p>Released: XX</p>
+        <p>Last Modified: XX</p>
+      </div>
+    );
+  }
+}
+
+class CurrentTask extends React.Component {
+  render() {
+    return <div>Study Task</div>;
+  }
+}
+
+class DeckProgress extends React.Component {
+  render() {
+    return <div>Progress</div>;
+  }
+}
+
 class AppBodyLeftSidebar extends React.Component {
   render() {
-    return <div className="app-body-left-sidebar">Left Sidebar</div>;
+    return (
+      <div className="app-body-left-sidebar">
+        <DeckInfo />
+      </div>
+    );
   }
 }
 
 class AppBodyCenter extends React.Component {
   render() {
-    return <div className="app-body-center">Center</div>;
+    return (
+      <div className="app-body-center">
+        <CurrentTask />
+      </div>
+    );
   }
 }
 
 class AppBpdyRightSidebar extends React.Component {
   render() {
-    return <div className="app-body-right-sidebar">Right Sidebar</div>;
+    return (
+      <div className="app-body-right-sidebar">
+        <DeckProgress />
+      </div>
+    );
   }
 }
 
