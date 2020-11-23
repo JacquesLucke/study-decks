b2cb2d10d1033d8eee346f62dd3aeaf39be1ee59
Jacques Lucke <mail@jlucke.com>
2020-11-23 22:49:03 +0100

    use function instead of class components

--- web-frontend/src/index.tsx
+++ web-frontend/src/index.tsx
@@ -2,76 +2,64 @@ import * as React from "react";
 import * as ReactDom from "react-dom";
 import "./app-style.scss";
 
-class AppHeader extends React.Component {
-  render() {
-    return (
-      <div className="app-header">
-        <div className="app-header-content">
-          <div className="app-header-name">Study Decks</div>
-          <div className="app-header-right">
-            <nav>
-              <ul>
-                <li>Log In</li>
-                <li>About</li>
-                <li>Home</li>
-              </ul>
-            </nav>
-          </div>
+function App() {
+  return (
+    <div>
+      <AppHeader />
+      <AppBody />
+    </div>
+  );
+}
+
+function AppHeader() {
+  return (
+    <div className="app-header">
+      <div className="app-header-content">
+        <div className="app-header-name">Study Decks</div>
+        <div className="app-header-right">
+          <nav>
+            <ul>
+              <li>Log In</li>
+              <li>About</li>
+              <li>Home</li>
+            </ul>
+          </nav>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
-class DeckInfo extends React.Component {
-  render() {
-    return (
-      <div className="deck-info">
-        <p>You are learning:</p>
-        <p>How to do X?</p>
-        <hr />
-        <p>Author: Some Name</p>
-        <p>Questions: XX</p>
-        <p>Released: XX</p>
-        <p>Last Modified: XX</p>
-      </div>
-    );
-  }
+function AppBody() {
+  return (
+    <div className="app-body">
+      <DeckInfo />
+      <DeckTask />
+      <DeckProgress />
+    </div>
+  );
 }
 
-class DeckTask extends React.Component {
-  render() {
-    return <div className="deck-task">Study Task</div>;
-  }
+function DeckInfo() {
+  return (
+    <div className="deck-info">
+      <p>You are learning:</p>
+      <p>How to do X?</p>
+      <hr />
+      <p>Author: Some Name</p>
+      <p>Questions: XX</p>
+      <p>Released: XX</p>
+      <p>Last Modified: XX</p>
+    </div>
+  );
 }
 
-class DeckProgress extends React.Component {
-  render() {
-    return <div className="deck-progress">Progress</div>;
-  }
+function DeckTask() {
+  return <div className="deck-task">Study Task</div>;
 }
 
-class AppBody extends React.Component {
-  render() {
-    return (
-      <div className="app-body">
-        <DeckInfo />
-        <DeckTask />
-        <DeckProgress />
-      </div>
-    );
-  }
-}
-
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <AppHeader />
-        <AppBody />
-      </div>
-    );
-  }
+function DeckProgress() {
+  return <div className="deck-progress">Progress</div>;
 }
 
 const container = document.querySelector("#app-div");
