f38852cb03bfae067975843145ca35b19837f4f8
Jacques Lucke <mail@jlucke.com>
2021-01-01 15:29:43 +0100

    static file server for web frontend

--- app-server/src/main.ts
+++ app-server/src/main.ts
@@ -1,19 +1,36 @@
 import * as Hapi from "@hapi/hapi";
+import * as Path from "path";
 
 process.on("unhandledRejection", (err) => {
   console.log(err);
   process.exit(1);
 });
 
+const public_directory = Path.dirname(Path.dirname(__dirname));
+const web_frontend_dist_directory = Path.join(
+  public_directory,
+  "web-frontend",
+  "dist"
+);
+console.log("Public Directory: " + public_directory);
+
 /* Use init function, because await cannot be used outside of functions yet. */
 const init_server = async () => {
-  const server = Hapi.server({ port: 3000, host: "0.0.0.0" });
+  const server = Hapi.server({
+    port: 3000,
+    host: "0.0.0.0",
+  });
+  await server.register(require("@hapi/inert"));
+
+  server.path(public_directory);
 
   server.route({
     method: "GET",
-    path: "/",
-    handler: (request) => {
-      return "Hello World!";
+    path: "/{param*}",
+    handler: {
+      directory: {
+        path: web_frontend_dist_directory,
+      },
     },
   });
 
