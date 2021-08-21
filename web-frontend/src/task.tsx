1d1a53d78b38fb0718e39569c96fead9de0aad1b
Jacques Lucke <mail@jlucke.com>
2021-08-21 13:43:47 +0200

    add textbox

--- web-frontend/src/task.tsx
+++ web-frontend/src/task.tsx
@@ -3,9 +3,20 @@ import styled from "styled-components";
 
 export default function Task() {
   return (
-    <StyledTaskBody>
-      How are countries generally classified into rich and poor?
-    </StyledTaskBody>
+    <>
+      <StyledTaskBody>
+        How are countries generally classified into rich and poor?
+      </StyledTaskBody>
+      <TextArea />
+    </>
+  );
+}
+
+function TextArea() {
+  return (
+    <StyledTextAreaContainer>
+      <StyledTextArea rows={5} />
+    </StyledTextAreaContainer>
   );
 }
 
@@ -15,4 +26,24 @@ const StyledTaskBody = styled.article`
   font-family: "Roboto";
   font-size: 20pt;
   padding: 0 1em;
+`;
+
+const StyledTextAreaContainer = styled.div`
+  margin: 1em auto;
+  max-width: 60rem;
+  padding-left: 1em;
+  padding-right: 1em;
+`;
+
+const StyledTextArea = styled.textarea`
+  display: block;
+  background-color: #f8f8f8;
+  width: 100%;
+  box-sizing: border-box;
+  font-family: "Roboto";
+  font-size: 16pt;
+  resize: vertical;
+  padding: 0.2em;
+  border-radius: 0.2em;
+  border: 1px solid #aaa;
 `;
